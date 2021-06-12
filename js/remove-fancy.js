dc6054205ec9894eeb173d9794a8bcb6c0112e68
davebrogan <dave@davebrogan.com>
2021-06-11 18:02:41 -0600

    finished contact strip and pushed to dev on site manager

---
+++ js/remove-fancy.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.parent-element-class-name').find('.fancy-link').removeClass('fancy-link');
+    $('.parent-element-class-name').find('.fancy-link').removeClass('fancy-link');
+});
+
+//place at end of body html
