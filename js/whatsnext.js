6711628451f1023234d251a642a6e44d03870d02
davebrogan <dave@davebrogan.com>
2021-06-18 17:06:49 -0600

    added dev, backup and deploy copies of every html file. added structures and edit proceedures to maintenance-instructions.txt

---
+++ js/whatsnext.js
@@ -0,0 +1,48 @@
+<!-- Hotjar Tracking Code for weber.edu -->
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1987894,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+$(document).ready(function(){
+    $('main').find('.fancy-link').removeClass('fancy-link');
+    $('main').find('.fancy-link').removeClass('fancy-link');
+
+/*    $("#menu-item-step1").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#step1-scroll").offset().top
+        },1000);
+    });*/
+
+    $("#menu-item-step1").keyup(function(event) {
+        if (event.which === 13) {
+            $(this).click(step1Scroll());
+        }
+    });
+
+    var step1Scroll = function() {
+        $('html, body').animate({
+            scrollTop: $("#step1-scroll").offset().top
+        }, 1000);
+    };
+
+    $("#submit").click(function() {
+        alert('clicked!');
+    });
+
+    $("#menu-item-step2").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#step2-scroll").offset().top
+        },1000);
+    });
+
+    $("#menu-item-step3").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#step3-scroll").offset().top
+        },1000);
+    });
+});
